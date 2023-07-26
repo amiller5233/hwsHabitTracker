9a575b232ea782e9476d33fb941c25c767f78daf
amiller5233@gmail.com <amiller5233@gmail.com>
2023-07-25 17:29:37 -0700

    create xcode project

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Adam Miller on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
